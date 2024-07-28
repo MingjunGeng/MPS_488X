ff7b0319fa0bf7122aa6074f3b5b3daa53059fd0
minjun.geng@gmail.com <sooofeye@Sooofs-Mac-mini.attlocal.net>
2024-07-27 22:36:15 -0700

    def device

--- README.md
+++ README.md
@@ -11,43 +11,35 @@
 └── v01                             ---> tag v01
 ```
 
-## menu 
-```
-Main Menu
-0.  Normal_Mode_Update               ---> Update Device Normal Mode Fireware
-1.  Recovery_Mode_Update             ---> Update Device Recovery Mode Fireware
-2.  Mac_Update                       ---> Change Mac Sddress and Serial Number 
-3.  Mac_Check                        ---> Change Mac Sddress and Serial Number 
-```
 ## run cmd
 ```
 #python3 mac_main.py 
 Main Menu
-0.  Normal_Mode_Update
-1.  Recovery_Mode_Update
-2.  Mac_Update
-3.  Mac_Check
+0.  Update Device Normal Mode Fireware
+1.  Update Device Recovery Mode Fireware
+2.  Change Mac Sddress and Serial Number
+3.  Change Mac Sddress and Serial Number
 
 Plase enter you  choice: 0
 Main Menu
-0.  Normal_Mode_Update
-1.  Recovery_Mode_Update
-2.  Mac_Update
-3.  Mac_Check
+0.  Update Device Normal Mode Fireware
+1.  Update Device Recovery Mode Fireware
+2.  Change Mac Sddress and Serial Number
+3.  Change Mac Sddress and Serial Number
 
 Plase enter you  choice: 1
 Main Menu
-0.  Normal_Mode_Update
-1.  Recovery_Mode_Update
-2.  Mac_Update
-3.  Mac_Check
+0.  Update Device Normal Mode Fireware
+1.  Update Device Recovery Mode Fireware
+2.  Change Mac Sddress and Serial Number
+3.  Change Mac Sddress and Serial Number
 
 Plase enter you  choice: 2
 Main Menu
-0.  Normal_Mode_Update
-1.  Recovery_Mode_Update
-2.  Mac_Update
-3.  Mac_Check
+0.  Update Device Normal Mode Fireware
+1.  Update Device Recovery Mode Fireware
+2.  Change Mac Sddress and Serial Number
+3.  Change Mac Sddress and Serial Number
 
 Plase enter you  choice: 3
 ```
--- mac_main.py
+++ mac_main.py
@@ -4,105 +4,111 @@ import time
 import pyautogui
 import keyboard
 
-def Normal_Mode_Update():
-    pyautogui.size()
-    width, height = pyautogui.size()
-    # login
-    pyautogui.moveTo(1450, 300, duration=0.25)
-    pyautogui.click()
-    pyautogui.moveTo(1650, 110, duration=0.25)
-    pyautogui.click()
+class MPS_488X():
+    def __init__(self):
+        pass
+    def test(self):
+        print("######")
+        return 'TEST'
+    def Normal_Mode_Update(self):
+        pyautogui.size()
+        width, height = pyautogui.size()
+        # login
+        pyautogui.moveTo(1450, 300, duration=0.25)
+        pyautogui.click()
+        pyautogui.moveTo(1650, 110, duration=0.25)
+        pyautogui.click()
 
-    # Browse
-    pyautogui.moveTo(1300, 350, duration=0.25)
-    pyautogui.click()
-    pyautogui.moveTo(1300, 145, duration=0.25)
-    pyautogui.click()
-    # open
-    pyautogui.moveTo(1750, 563, duration=0.25)
-    pyautogui.click()
-    #
+        # Browse
+        pyautogui.moveTo(1300, 350, duration=0.25)
+        pyautogui.click()
+        pyautogui.moveTo(1300, 145, duration=0.25)
+        pyautogui.click()
+        # open
+        pyautogui.moveTo(1750, 563, duration=0.25)
+        pyautogui.click()
+        #
 
-    pyautogui.moveTo(1400, 395, duration=0.25)
-    pyautogui.click()
+        pyautogui.moveTo(1400, 395, duration=0.25)
+        pyautogui.click()
 
-    # Yes
-    pyautogui.moveTo(1300, 655, duration=0.25)
-    pyautogui.click()
+        # Yes
+        pyautogui.moveTo(1300, 655, duration=0.25)
+        pyautogui.click()
 
-def Recovery_Mode_Update():
-    pyautogui.size()
-    width, height = pyautogui.size()
-    pyautogui.moveTo(1450, 300, duration=0.25)
-    pyautogui.click()
-    pyautogui.moveTo(1650, 110, duration=0.25)
-    pyautogui.click()
-    # Browse
-    pyautogui.moveTo(1300, 550, duration=0.25)
-    pyautogui.click()
-    pyautogui.moveTo(1300, 145, duration=0.25)
-    pyautogui.click()
-    # open
-    pyautogui.moveTo(1750, 563, duration=0.25)
-    pyautogui.click()
-    #
-    pyautogui.moveTo(1300, 600, duration=0.25)
-    pyautogui.click()
+    def Recovery_Mode_Update(self):
+        pyautogui.size()
+        width, height = pyautogui.size()
+        pyautogui.moveTo(1450, 300, duration=0.25)
+        pyautogui.click()
+        pyautogui.moveTo(1650, 110, duration=0.25)
+        pyautogui.click()
+        # Browse
+        pyautogui.moveTo(1300, 550, duration=0.25)
+        pyautogui.click()
+        pyautogui.moveTo(1300, 145, duration=0.25)
+        pyautogui.click()
+        # open
+        pyautogui.moveTo(1750, 563, duration=0.25)
+        pyautogui.click()
+        #
+        pyautogui.moveTo(1300, 600, duration=0.25)
+        pyautogui.click()
 
-    # Yes
-    pyautogui.moveTo(1300, 655, duration=0.25)
-    pyautogui.click()
+        # Yes
+        pyautogui.moveTo(1300, 655, duration=0.25)
+        pyautogui.click()
 
-def Mac_Update():
-    os.startfile(r"C:\Dell\MAC\MPS-488X.lnk")
+    def Mac_Update(self):
+        os.startfile(r"C:\Dell\MAC\MPS-488X.lnk")
 
-    time.sleep(2)
-    pyautogui.typewrite("\n")
-    pyautogui.typewrite("FIV?\n")
-    #time.sleep(10)
-    pyautogui.press("enter")
-    pyautogui.typewrite("TEST MPSTryToGuess42\n")
-    pyautogui.press("enter")
-    pyautogui.typewrite("OTP M\n")
-    pyautogui.press("enter")
-    pyautogui.typewrite("OTP S\n")
-    pyautogui.press("enter")
+        time.sleep(2)
+        pyautogui.typewrite("\n")
+        pyautogui.typewrite("FIV?\n")
+        #time.sleep(10)
+        pyautogui.press("enter")
+        pyautogui.typewrite("TEST MPSTryToGuess42\n")
+        pyautogui.press("enter")
+        pyautogui.typewrite("OTP M\n")
+        pyautogui.press("enter")
+        pyautogui.typewrite("OTP S\n")
+        pyautogui.press("enter")
 
-    pyautogui.typewrite("SER ")
-    keyboard.wait("enter")
-    pyautogui.typewrite("\n")
-    pyautogui.typewrite("\n")
-    pyautogui.typewrite("SER\n")
-    pyautogui.press("enter")
-    pyautogui.typewrite("MAC ")
-    keyboard.wait("enter")
-    pyautogui.typewrite("\n")
-    pyautogui.typewrite("\n")
-    pyautogui.typewrite("MAC\n")
-    keyboard.wait("enter")
-    pyautogui.typewrite("OTP O\n")
-    keyboard.wait("enter")
-    pyautogui.typewrite("RESET_FACTORY\n")
-    pyautogui.press("enter")
-    pyautogui.typewrite("exit()\n")
+        pyautogui.typewrite("SER ")
+        keyboard.wait("enter")
+        pyautogui.typewrite("\n")
+        pyautogui.typewrite("\n")
+        pyautogui.typewrite("SER\n")
+        pyautogui.press("enter")
+        pyautogui.typewrite("MAC ")
+        keyboard.wait("enter")
+        pyautogui.typewrite("\n")
+        pyautogui.typewrite("\n")
+        pyautogui.typewrite("MAC\n")
+        keyboard.wait("enter")
+        pyautogui.typewrite("OTP O\n")
+        keyboard.wait("enter")
+        pyautogui.typewrite("RESET_FACTORY\n")
+        pyautogui.press("enter")
+        pyautogui.typewrite("exit()\n")
 
-def Mac_Check():
-    os.startfile(r"C:\Dell\MAC\MPS-488X.lnk")
-    time.sleep(2)
-    pyautogui.typewrite("\n")
-    pyautogui.typewrite("FIV?\n")
-    pyautogui.press("enter")
+    def Mac_Check(self):
+        os.startfile(r"C:\Dell\MAC\MPS-488X.lnk")
+        time.sleep(2)
+        pyautogui.typewrite("\n")
+        pyautogui.typewrite("FIV?\n")
+        pyautogui.press("enter")
 
-    pyautogui.typewrite("TEST MPSTryToGuess42\n")
-    pyautogui.press("enter")
+        pyautogui.typewrite("TEST MPSTryToGuess42\n")
+        pyautogui.press("enter")
 
-    pyautogui.typewrite("OTP M\n")
-    pyautogui.press("enter")
+        pyautogui.typewrite("OTP M\n")
+        pyautogui.press("enter")
 
-    pyautogui.typewrite("OTP S\n")
-    keyboard.wait("enter")
+        pyautogui.typewrite("OTP S\n")
+        keyboard.wait("enter")
 
-    pyautogui.typewrite("exit()\n")
+        pyautogui.typewrite("exit()\n")
    
 def run_menu(menu, parent=None):
     while True:
@@ -118,15 +124,17 @@ def print_menu(menu):
         #print(i, option['text'])
         print(f"{i}. {option['text']}")
     print()
-
+mps_488X = MPS_488X()
+#print("test", mps_488X.test())
 COMMAND = 'command'
 main_menu = {
     'title':  'Main Menu',
     'options': [
-        {'type': COMMAND, 'text': ' Normal_Mode_Update', 'command': Normal_Mode_Update },
-        {'type': COMMAND, 'text': ' Recovery_Mode_Update', 'command': Recovery_Mode_Update},
-        {'type': COMMAND, 'text': ' Mac_Update', 'command': Mac_Update},
-        {'type': COMMAND, 'text': ' Mac_Check', 'command': Mac_Check},
+        {'type': COMMAND, 'text': ' Update Device Normal Mode Fireware', 'command': mps_488X.Normal_Mode_Update },
+        {'type': COMMAND, 'text': ' Update Device Recovery Mode Fireware', 'command': mps_488X.Recovery_Mode_Update},
+        {'type': COMMAND, 'text': ' Change Mac Sddress and Serial Number', 'command': mps_488X.Mac_Update},
+        {'type': COMMAND, 'text': ' Change Mac Sddress and Serial Number', 'command': mps_488X.Mac_Check},
+        #{'type': COMMAND, 'text': ' EXIT', 'command': mps_488X.test},
         ]
     }
 run_menu(main_menu)
